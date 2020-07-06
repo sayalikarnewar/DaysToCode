f4f342af98dc9f9a1dce1fd2c5e499b07113373a
SayaliKarnewar <33855068+sayalikarnewar@users.noreply.github.com>
2020-07-06 11:08:44 +0530

    Create Calendar.py

---
+++ Calendar.py
@@ -0,0 +1,8 @@
+#Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+
+data = (11,12,2004)
+data = list(data)
+print(*data, sep="/")
+
+#output
+#11/12/2004
